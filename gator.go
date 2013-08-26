f74794222232637806875d3846f394841e2e7d54
Harry Jeffery <harry@exec64.co.uk>
2013-08-26 20:42:06 +0100

    Changed i/o methods to use io.Writers/io.Readers instead of []byte

--- gator.go
+++ gator.go
@@ -37,18 +37,8 @@ func main() {
 func handleConnection(client net.Conn) {
 	defer client.Close()
 
-	//Maximum length the packet can be is 2 + 255
-	buf := make([]byte, 257)
-
-	//First we get the method request
-	_, err := client.Read(buf)
-	if err != nil {
-		log.Printf("Error reading version header: %s", err.Error())
-		return
-	}
-
 	mr := new(MethodRequest)
-	if err := mr.Read(buf); err != nil {
+	if err := mr.ReadBinary(client); err != nil {
 		log.Printf(err.Error())
 		return
 	}
@@ -66,24 +56,17 @@ func handleConnection(client net.Conn) {
 		rep := new(MethodReply)
 		rep.version = 0x05
 		rep.method = 0x00 //No auth required
-		client.Write(rep.bytes())
+		rep.WriteBinary(client)
 	} else {
 		rep := new(MethodReply)
 		rep.version = 0x05
 		rep.method = 0xFF //No method available
-		client.Write(rep.bytes())
-		return
-	}
-
-	//Read their socks request
-	_, err = client.Read(buf)
-	if err != nil {
-		log.Printf("Error reading socks request: %s", err.Error())
+		rep.WriteBinary(client)
 		return
 	}
 
 	sr := new(SocksRequest)
-	if err := sr.Read(buf); err != nil {
+	if err := sr.ReadBinary(client); err != nil {
 		log.Printf(err.Error())
 		return
 	}
@@ -98,7 +81,7 @@ func handleConnection(client net.Conn) {
 		srep.addressType = sr.addressType
 		srep.address = sr.address
 		srep.port = sr.port
-		client.Write(srep.bytes())
+		srep.WriteBinary(client)
 		return
 	}
 
@@ -124,7 +107,7 @@ func handleConnection(client net.Conn) {
 		srep.addressType = sr.addressType
 		srep.address = sr.address
 		srep.port = sr.port
-		client.Write(srep.bytes())
+		srep.WriteBinary(client)
 		return
 	}
 	defer server.Close()
@@ -137,7 +120,7 @@ func handleConnection(client net.Conn) {
 	srep.address = sr.address
 	srep.domain = sr.domain
 	srep.port = sr.port
-	client.Write(srep.bytes())
+	srep.WriteBinary(client)
 
 	stopChan := make(chan bool)
 	go func() {
@@ -188,13 +171,14 @@ type SocksReply struct {
 	port        int
 }
 
-func (s MethodRequest) Read(b []byte) error {
-	if len(b) < 3 || len(b) < int(2+b[1]) {
+func (s MethodRequest) ReadBinary(r io.Reader) error {
+	b := make([]byte, 2)
+	if n, _ := r.Read(b); n != 2 {
 		return errors.New("method request is too short")
-	} else if b[0] != 0x05 {
+	}
+
+	if s.version = b[0]; s.version != 0x05 {
 		return errors.New(fmt.Sprintf("Invalid version: %d", b[0]))
-	} else {
-		s.version = b[0]
 	}
 
 	if b[1] == 0 {
@@ -203,70 +187,97 @@ func (s MethodRequest) Read(b []byte) error {
 		s.methods = make([]byte, b[1])
 	}
 
-	methodBytes := b[2 : 2+b[1]]
-	copy(s.methods, methodBytes)
+	if _, err := r.Read(s.methods); err != nil {
+		return fmt.Errorf("Error reading MethodRequest: %v", err)
+	}
 
 	return nil
 }
 
-func (s MethodReply) bytes() []byte {
-	return []byte{s.version, s.method}
+func (s MethodReply) WriteBinary(w io.Writer) error {
+	out := []byte{s.version, s.method}
+	if _, err := w.Write(out); err != nil {
+		return fmt.Errorf("Error writing MethodReply: %v", err)
+	}
+
+	return nil
 }
 
-func (s SocksRequest) Read(b []byte) error {
-	if len(b) < 5 {
-		return errors.New("socks request is too short")
+func (s SocksRequest) ReadBinary(r io.Reader) error {
+	b := make([]byte, 4)
+
+	n, err := r.Read(b)
+	if n < 4 {
+		return errors.New("SocksRequest input too short")
 	}
 
-	if b[0] != 0x05 {
-		return errors.New(fmt.Sprintf("Invalid version: %d", b[0]))
-	} else {
-		s.version = b[0]
+	if err != nil {
+		return fmt.Errorf("Error reading SocksRequest: %v", err)
 	}
 
-	if b[1] < 1 || b[1] > 3 {
-		return errors.New(fmt.Sprintf("Invalid command: %d", b[1]))
-	} else {
-		s.command = b[1]
+	if s.version = b[0]; s.version != 0x05 {
+		return fmt.Errorf("Invalid version: %d", b[0])
 	}
 
-	//b[2] == 0x00 and is reserved
-
-	if b[3] == 0 || b[3] == 2 || b[3] > 4 {
-		return errors.New(fmt.Sprintf("Invalid address type: %d", b[3]))
-	} else {
-		s.addressType = b[3]
+	if s.command = b[1]; b[1] == 0 || b[1] > 3 {
+		return fmt.Errorf("Invalid command: %d", s.command)
 	}
 
-	offset := 0
+	if s.addressType = b[3]; b[3] != 1 && b[3] != 3 && b[3] != 4 {
+		return fmt.Errorf("Invalid address type: %d", b[3])
+	}
 
 	if s.addressType == 1 {
-		if len(b) < 10 {
-			return errors.New("socks request is too short")
+		address := make([]byte, 4)
+		if n, err = r.Read(address); n < 4 {
+			return errors.New("SocksRequest address is too short")
 		}
-		s.address = b[4:8]
-		offset = 8
+
+		if err != nil {
+			return fmt.Errorf("Error reading SockRequest.Address: %v", err)
+		}
+
+		s.address = address
+
 	} else if s.addressType == 4 {
-		if len(b) < 22 {
-			return errors.New("socks request is too short")
+		address := make([]byte, 16)
+		if n, err = r.Read(address); n < 16 {
+			return errors.New("SocksRequest address is too short")
 		}
-		s.address = b[4:20]
-		offset = 20
+
+		if err != nil {
+			return fmt.Errorf("Error reading SockRequest.Address: %v", err)
+		}
+
+		s.address = address
+
 	} else if s.addressType == 3 {
-		domainLength := b[4]
-		if len(b) < int(7)+int(domainLength) {
-			return errors.New("socks request is too short")
+		length := make([]byte, 1)
+
+		if _, err = r.Read(length); err != nil {
+			return fmt.Errorf("Error reading SockRequest domain length: %v", err)
 		}
-		s.domain = string(b[5 : 5+domainLength])
-		offset = int(5) + int(domainLength)
+
+		domain := make([]byte, int(length[0]))
+		n, err = r.Read(domain)
+		if n < int(length[0]) {
+			return errors.New("SocksRequest domain too short")
+		}
+
+		s.domain = string(domain)
+
+		if err != nil {
+			return fmt.Errorf("Error reading SockRequest domain: %v", err)
+		}
 	}
 
-	s.port = (int(b[offset]) << 8) + int(b[offset+1])
+	//TODO
+	//s.port = (int(b[offset]) << 8) + int(b[offset+1])
 
 	return nil
 }
 
-func (s SocksReply) bytes() []byte {
+func (s SocksReply) WriteBinary(w io.Writer) error {
 	var b []byte
 	if s.addressType == 0 || s.addressType == 1 {
 		b = make([]byte, 0, 10)
@@ -284,8 +295,12 @@ func (s SocksReply) bytes() []byte {
 		b = append(b, s.domain...)
 		b = append(b, byte((s.port&0xFF00)>>8), byte(s.port&0xFF))
 	} else {
-		return nil
+		return errors.New("Cannot write SocksReply, Invalid address type")
 	}
 
-	return b
+	if _, err := w.Write(b); err != nil {
+		return fmt.Errorf("Error writing SocksReply: %v", err)
+	}
+
+	return nil
 }
